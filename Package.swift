d0828dd83b7dba9bae022c8cae22fd239542ce2c
Mingwan Choi <wbsldj7645@naver.com>
2025-03-27 15:35:08 +0900

    check uwb connect

--- Package.swift
+++ Package.swift
@@ -5,17 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "GrowSpaceUWBSDK",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GrowSpaceUWBSDK",
             targets: ["GrowSpaceUWBSDK"]),
     ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "GrowSpaceUWBSDK"),
+            name: "GrowSpaceUWBSDK",
+            dependencies: ["GrowSpaceUWBPrivateSDK"]
+        ),
+        
+        .binaryTarget(
+            name: "GrowSpaceUWBPrivateSDK",
+            url: "https://github.com/freegrowenterprise/SpaceUWBSDK-iOS/releases/download/v0.0.1/GrowSpaceUWBPrivateSDK.xcframework.zip",
+            checksum: "93fb73d1c150785ee3eeb24fc7f8ad463301b8cb0bd8fbe5cafc6e831d61c7cc"
+        ),
         .testTarget(
             name: "GrowSpaceUWBSDKTests",
             dependencies: ["GrowSpaceUWBSDK"]
--- Sources/GrowSpaceUWBSDK/GrowSpaceUWBSDK.swift
+++ Sources/GrowSpaceUWBSDK/GrowSpaceUWBSDK.swift
@@ -1,2 +1,18 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import GrowSpaceUWBPrivateSDK
+
+public class GrowSpaceUWBSDK {
+    private var spaceUwb: SpaceUWB
+    
+    init(spaceUwb: SpaceUWB) {
+        self.spaceUwb = spaceUwb
+    }
+    
+    func connect() {
+        self.spaceUwb.spaceUWBHandler = {
+            print("UWB 연결 결과 : \($0)")
+        }
+    }
+}
